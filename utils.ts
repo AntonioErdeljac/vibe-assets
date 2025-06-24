8d6a08c46096167d466788f9ae989c105fdbd78a
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2025-06-24 04:36:11 +0200

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { TreeItem } from "@/components/tree-view";
+import { type TreeItem } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
